118cff519e18abc1b15428cda1bfba8dfd93b2d1
Agyeiwaa-git <laosei023@st.ug.edu.gh>
2026-07-27 15:40:49 +0000

    Complete question 3

--- assignment_03_array_statistics.py
+++ assignment_03_array_statistics.py
@@ -39,3 +39,48 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+
+
+n = int(input("How many numbers? "))
+if n <= 0:
+    print("Error:Number of values must be positive.")
+else:
+    numbers = []
+
+    for i in range(n):
+        number = int(input(f"Enter number {i + 1}: "))
+        numbers.append(number)
+
+def calculate_sum(numbers):
+    total = 0 
+    for number in numbers:
+        total += number
+    return total
+
+def calculate_average(numbers):
+    total = calculate_sum(numbers)
+    return total / len(numbers)
+
+def calculate_maximum(numbers):
+    maximum = numbers[0]
+
+    for number in numbers:
+        if number > maximum:
+            maximum = number
+
+    return maximum
+
+def calculate_minimum(numbers):
+    minimum = numbers[0]
+
+    for number in numbers:
+        if number < minimum:
+            minimum = number
+
+    return minimum
+
+print("\nResults:")
+print("Sum:",calculate_sum(numbers))
+print("Average:",calculate_average(numbers))
+print("Maximum:",calculate_maximum(numbers))
+print("Minimum:",calculate_minimum(numbers))
